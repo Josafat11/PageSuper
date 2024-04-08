d444a57de39f52493fec950433aa4b1b528b4184
Josafat11 <116617414+Josafat11@users.noreply.github.com>
2024-04-08 10:26:46 -0600

    cambios

--- src/config/confing.jsx
+++ src/config/confing.jsx
@@ -1,4 +1,4 @@
 export const CONFIGURACIONES={
-    BASEURL:'https://apiback-eight.vercel.app/api',
+    BASEURL:'https://apiback-one.vercel.app/api',
     BASEURL2:'http://192.168.1.69:4000/api'
 }
